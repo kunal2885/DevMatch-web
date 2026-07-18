0bfaf80ead304ab776249b1b273a4383b99cc19a
kunal2885 <kunalphogat38@gmail.com>
2026-07-18 17:07:49 +0530

    implemented dispatch and useSelector hook and cond rendering of profile picture and redirection to feed after login

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,18 +2,22 @@ import React, { useState } from 'react'
 import axios from 'axios'
 import { useDispatch } from 'react-redux'
 import {addUser} from "./utils/userSlice"
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
   const [emailid , setEmailId] = useState("kushal@gmail.com")
   const [password , setPassword] = useState("Kushal@123")
   const dispatch = useDispatch()
+  const navigate = useNavigate()
   const handleLogin = async ()=>{
     const res = await axios.post("http://localhost:7777/login",{
       emailid,
       password
     },{withCredentials : true})
-    console.log(res.data)
+    
     dispatch(addUser(res.data))
+
+    return navigate("/")
 
   }
   return (
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+
 
 const Navbar = () => {
+  const user = useSelector((state ) => state.user)
+  
   return (
     
       <div className="navbar bg-base-300 shadow-sm">
@@ -9,37 +13,47 @@ const Navbar = () => {
         </div>
         <div className="flex gap-2">
           
-          <div className="dropdown dropdown-end mx-5">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle avatar"
-            >
-              <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                />
-              </div>
-            </div>
-            <ul
-              tabIndex="-1"
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow "
-            >
-              <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
-              <li>
-                <a>Settings</a>
-              </li>
-              <li>
-                <a>Logout</a>
-              </li>
-            </ul>
-          </div>
+          {
+  user && (
+    <div className="dropdown dropdown-end mx-5">
+      <div
+        tabIndex={0}
+        role="button"
+        className="btn btn-ghost flex items-center gap-3 px-2"
+      >
+        <p className="font-medium whitespace-nowrap">
+          Welcome, {user.firstname}
+        </p>
+
+        <div className="w-10 h-10 rounded-full overflow-hidden">
+          <img
+            className="w-full h-full object-cover"
+            src={user.photourl}
+            alt="Profile"
+          />
+        </div>
+      </div>
+
+      <ul
+        tabIndex={-1}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2 shadow"
+      >
+        <li>
+          <a className="justify-between">
+            Profile
+            <span className="badge">New</span>
+          </a>
+        </li>
+        <li>
+          <a>Settings</a>
+        </li>
+        <li>
+          <a>Logout</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
         </div>
       </div>
     
