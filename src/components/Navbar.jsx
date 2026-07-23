eb72c10d237be62f3d55f3133b991582af3475dd
kunal2885 <kunalphogat38@gmail.com>
2026-07-23 14:55:20 +0530

    redirect if not login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,8 @@
+//import Link from 'daisyui/components/link'
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector} from 'react-redux'
+import { Link } from 'react-router-dom'
+
 
 
 const Navbar = () => {
@@ -9,7 +12,7 @@ const Navbar = () => {
     
       <div className="navbar bg-base-300 shadow-sm">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">DevTinder</a>
+          <Link to="/" className="btn btn-ghost text-xl">DevTinder</Link>
         </div>
         <div className="flex gap-2">
           
@@ -39,16 +42,16 @@ const Navbar = () => {
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2 shadow"
       >
         <li>
-          <a className="justify-between">
+          <Link to="/profile" className="justify-between">
             Profile
             <span className="badge">New</span>
-          </a>
+          </Link>
         </li>
         <li>
-          <a>Settings</a>
+          <Link>Settings</Link>
         </li>
         <li>
-          <a>Logout</a>
+          <Link>Logout</Link>
         </li>
       </ul>
     </div>
