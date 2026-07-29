31a64be7dddec04cab99da288dae3dffcca4dc3b
kunal2885 <kunalphogat38@gmail.com>
2026-07-30 00:07:00 +0530

    feat:revie request implemeted

--- src/utils/requestsSlice.js
+++ src/utils/requestsSlice.js
@@ -8,12 +8,13 @@ const requestsSlice = createSlice({
         addRequests : (state,action)=>{
             return action.payload
         },
-        removeRequests : (state,action)=>{
-            return null
+        removeRequest : (state,action)=>{
+            const newArray = state.filter((request) => request._id != action.payload)
+            return newArray
         }
     }
 })
 
-export const {addRequests , removeRequests}= requestsSlice.actions
+export const {addRequests , removeRequest}= requestsSlice.actions
 
 export default requestsSlice.reducer
